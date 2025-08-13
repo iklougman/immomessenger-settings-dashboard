3ca4118519f979bb3a38376fc57a00750361e53b
Igor <igor.klougman@actineo.de>
2025-08-13 15:19:44 +0200

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="de" {...mantineHtmlProps}>
       <head>
         <ColorSchemeScript />
       </head>
